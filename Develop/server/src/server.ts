3f72ab9ff70649ca910008938c536a459fbe050d
Jethro <Jthao04@gmail.com>
2025-04-08 22:27:28 -0500

    lastone

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -4,20 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
-import cors from 'cors'; // Import the cors package
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// Configure CORS to allow requests from your frontend's domain
-const corsOptions = {
-  origin: 'https://kanban-board-1-sfy5.onrender.com', // Replace with your frontend's Render URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-  credentials: true, // Allow cookies if needed
-};
-app.use(cors(corsOptions)); // Apply the CORS middleware
 
 // Serves static files in the entire client's dist folder
 app.use(express.static('../client/dist'));
@@ -25,7 +16,7 @@ app.use(express.static('../client/dist'));
 app.use(express.json());
 app.use(routes);
 
-sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
+sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
   });
